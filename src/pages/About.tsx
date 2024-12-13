de029f1a8fe4a62f040254fdef3b07fb1c081f3f
Indrajit Chatterjee <66139321+indrajitchatterjee@users.noreply.github.com>
2024-12-13 19:35:50 +0530

    Initial Commit

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,6 +2,9 @@ import React from "react";
 
 export default function About(){
     return(
-        <><h1>This is About Page</h1></>
+        <>
+            <h1>This is About Page</h1>
+            <p>This line is added.</p>
+        </>
     );
 }
